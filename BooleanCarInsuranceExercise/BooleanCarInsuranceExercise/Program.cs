b70ffb9a0c1175d8d7725171f25ead2388681051
rflstruthers <rflstruthers@gmail.com>
2019-08-29 09:16:02 -0700

    Boolean Car Insurance Exercise

---
+++ BooleanCarInsuranceExercise/BooleanCarInsuranceExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace BooleanCarInsuranceExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string ageStr = Console.ReadLine();
+            int age = Convert.ToInt32(ageStr);
+            bool over15 = age >= 15;
+
+            Console.WriteLine("Have you ever had a DUI? Please answer true or false.");
+            string duiStr = Console.ReadLine();
+            bool dui = Convert.ToBoolean(duiStr);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string ticketsStr = Console.ReadLine();
+            int tickets = Convert.ToInt32(ticketsStr);
+            bool ticketLimit = tickets < 3;
+
+            bool qualified = over15 && ticketLimit && (dui == false);
+            Console.WriteLine(qualified);
+
+            Console.ReadLine();
+        }
+    }
+}
