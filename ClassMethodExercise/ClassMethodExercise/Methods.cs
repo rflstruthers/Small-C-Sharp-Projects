1bce3e646a03d04a592a08346cc01c9cb4a5a2cf
rflstruthers <rflstruthers@gmail.com>
2019-09-10 14:21:53 -0700

    Class and Method Exercise

---
+++ ClassMethodExercise/ClassMethodExercise/Methods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise
+{
+    public class Methods
+    {
+        public static int Multiply(int input)
+        {
+            int timesTen = input * 10;
+            return timesTen;
+        }
+
+        public static int Add(int input)
+        {
+            int plusTen = input + 10;
+            return plusTen;
+        }
+
+        public static int Subtract(int input)
+        {
+            int minusTen = input - 10;
+            return minusTen;
+        }
+
+        public int Result { get; set; }
+    }
+}
---
+++ ClassMethodExercise/ClassMethodExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            
+
+            Console.WriteLine("Welcome to MATH! a program that performs mathematical operations on your favorite number.\n" +
+                "Please enter your favorite number:\n");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            int timesTen = Methods.Multiply(input);
+            int plusTen = Methods.Add(input);
+            int minusTen = Methods.Subtract(input);
+
+            Console.WriteLine(input + " x 10 = " + timesTen);
+            Console.WriteLine(input + " + 10 = " + plusTen);
+            Console.WriteLine(input + " - 10 = " + minusTen);
+
+
+            Console.ReadLine();
+        }
+    }
+}
