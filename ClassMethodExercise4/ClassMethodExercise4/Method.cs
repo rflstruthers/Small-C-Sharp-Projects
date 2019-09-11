abfe02abb4661e11ecbf82eef5c5cbddd3300827
rflstruthers <rflstruthers@gmail.com>
2019-09-11 12:10:05 -0700

    Class and Method Exercise #4
    Optional parameters

---
+++ ClassMethodExercise4/ClassMethodExercise4/Method.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise4
+{
+    public class Method
+    {
+        //method takes in two integers, one of which is optional, and returns an integer
+        public int OptionalMethod(int num1, int num2 = 0)
+        {
+            int result = (num1 * 10) + num2;
+            return result;
+        }
+
+        public int Result { get; set; }
+    }
+}
---
+++ ClassMethodExercise4/ClassMethodExercise4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method method = new Method();
+
+            Console.WriteLine("Please enter a number that you want multiplied by 10:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("If you wish, enter a number to be added to the result, you may leave blank if you wish:");
+
+            //Determines if second input is a number or not, if it is it is passed in to method as parameter "num2"
+            string input = Console.ReadLine();
+            int num2 = -1;
+            if (!int.TryParse(input, out num2))
+            {
+                Console.WriteLine(method.OptionalMethod(num1));
+            }
+            else
+            {
+                Console.WriteLine(method.OptionalMethod(num1, num2));
+
+            }
+
+
+            //if (string.IsNullOrEmpty(Console.ReadLine()))
+            //{
+            //    Console.WriteLine(method.OptionalMethod(num1));
+            //}
+            //else
+            //{
+            //    int num2 = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine(method.OptionalMethod(num1, num2));
+            //}
+           
+            Console.ReadLine();
+        }
+    }
+}
