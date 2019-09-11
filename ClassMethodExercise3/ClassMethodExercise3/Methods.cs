3e53307cb6a5c3548a0be3f54a8e0ed4ea9851ba
rflstruthers <rflstruthers@gmail.com>
2019-09-11 10:36:18 -0700

    Class and Method Exercise #3
    Dealing with overloads

---
+++ ClassMethodExercise3/ClassMethodExercise3/Methods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise3
+{
+    public class Methods
+    {
+        //method takes in an integer and returns an integer
+        public int MathMethod(int input)
+        {
+            int timesTen = input * 10;
+            return timesTen;
+        }
+
+        //method takes in a decimal and returns an integer
+        public int MathMethod(decimal input)
+        {
+            int divideTen = Convert.ToInt32(input / 10);
+            return divideTen;
+        }
+
+        //method takes in a string and returns an integer
+        public int MathMethod(string input)
+        {
+            int plusTen = (Int32.Parse(input)) + 10;
+            return plusTen;
+        }
+
+        public int Result { get; set; }
+    }
+}
---
+++ ClassMethodExercise3/ClassMethodExercise3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+            int timesTen = methods.MathMethod(33);
+            int divideTen = methods.MathMethod(33.33m);
+            int plusTen = methods.MathMethod("10");
+
+            Console.WriteLine("Integer input x 10 = " + timesTen);
+            Console.WriteLine("Decimal input / 10 = " + divideTen);
+            Console.WriteLine("String input + 10 = " + plusTen);
+
+            Console.ReadLine();
+        }
+    }
+}
