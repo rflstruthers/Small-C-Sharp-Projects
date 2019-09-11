87864119127979a6c9b37f97d8ebb1915528e226
rflstruthers <rflstruthers@gmail.com>
2019-09-11 09:24:32 -0700

    Class and Method Exercise #2

---
+++ ClassMethodExercise2/ClassMethodExercise2/Method.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise2
+{
+    public class Method
+    {
+        //void method doesnt return a value
+        //takes 2 integers as input parameters
+        public void AddTwo(int num1, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                int result = num1 + 2;
+            }
+            Console.WriteLine("We added 2 to {0}. We did it {1} times", num1, times);
+            
+            Console.ReadLine();
+        }
+
+        public int Result { get; set; }
+    }
+}
---
+++ ClassMethodExercise2/ClassMethodExercise2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate object "method" of class "Method"
+            Method method = new Method();
+
+            //call "AddTwo" method from class "Method"
+            method.AddTwo(num1 : 2, times: 5);
+        }
+    }
+}
