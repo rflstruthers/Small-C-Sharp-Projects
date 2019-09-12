8f2035bb795eb00e8956df58790f435d06931592
rflstruthers <rflstruthers@gmail.com>
2019-09-12 10:45:46 -0700

    Abstract Class Exercise

---
+++ AbstractClassExercise/AbstractClassExercise/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassExercise
+{
+    public class Employee : Person
+    {
+        //implement SayName method from Person class
+        public override void SayName()
+        {
+            Console.WriteLine("Hello, " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ AbstractClassExercise/AbstractClassExercise/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassExercise
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //method to be implemented in another class
+        public abstract void SayName();
+        
+    }
+}
