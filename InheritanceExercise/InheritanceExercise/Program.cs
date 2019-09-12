44efc1f3ddbc6b333329c54efb2cb8885885327e
rflstruthers <rflstruthers@gmail.com>
2019-09-12 09:14:31 -0700

    Inheritance Exercise

---
+++ InheritanceExercise/InheritanceExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instatiating Employee class with first and last name values
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            //calling superclass method SayName() on employee object
+            employee.SayName();
+
+
+            Console.ReadLine();
+        }
+    }
+}
