87578a16018987ec3a17cf5ce0eee0e45d56ca66
rflstruthers <rflstruthers@gmail.com>
2019-09-18 10:38:11 -0700

    Update

---
+++ StructExercise/StructExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number() { Amount = 5.00m };
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
