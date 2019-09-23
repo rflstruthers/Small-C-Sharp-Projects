302e5ba99a56749ad175b6e2218877c553bba5a6
rflstruthers <rflstruthers@gmail.com>
2019-09-23 11:46:31 -0700

    File I/O StreamWriter and StreamReader

--- InheritanceExercise/InheritanceExercise/Program.cs
+++ InheritanceExercise/InheritanceExercise/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace InheritanceAndInterfaceExercise
 {
@@ -10,52 +11,67 @@ namespace InheritanceAndInterfaceExercise
     {
         static void Main(string[] args)
         {
-            //Creating 10 employee objects with names and ID numbers
-            Employee employee1 = new Employee() { FirstName = "Joe", LastName = "Smith", Id = 001};
-            Employee employee2 = new Employee() { FirstName = "Sally", LastName = "Jones", Id = 002 };
-            Employee employee3 = new Employee() { FirstName = "Ralph", LastName = "Rodgers", Id = 003 };
-            Employee employee4 = new Employee() { FirstName = "Joe", LastName = "Green", Id = 004 };
-            Employee employee5 = new Employee() { FirstName = "Bob", LastName = "Allen", Id = 005 };
-            Employee employee6 = new Employee() { FirstName = "Murphy", LastName = "Palmer", Id = 006 };
-            Employee employee7 = new Employee() { FirstName = "Steve", LastName = "Stevenson", Id = 007 };
-            Employee employee8 = new Employee() { FirstName = "Patty", LastName = "Newman", Id = 008 };
-            Employee employee9 = new Employee() { FirstName = "Shirley", LastName = "Kipling", Id = 009 };
-            Employee employee10 = new Employee() { FirstName = "Greg", LastName = "Old", Id = 010 };
-
-            //Add all employees to a list
-            List<Employee> employees = new List<Employee>
+            //ask user for a number, log that number in a text file, read the number back to the user from the text file
+            Console.WriteLine("Please enter an employee ID number: ");
+            string id = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@".\log.txt", true))
             {
-                employee1,
-                employee2,
-                employee3,
-                employee4,
-                employee5,
-                employee6,
-                employee7,
-                employee8,
-                employee9,
-                employee10
-            };
-
-            //loop through list of employees and add all employees named "Joe" to a new list
-            //Using foreach
-            foreach (Employee employee in employees)
+                file.WriteLine(id);
+            }
+            using (StreamReader file = new StreamReader(@".\log.txt", true))
             {
-                if (employee.FirstName == "Joe")
-                {
-                    List<Employee> foreachJoes = new List<Employee>();
-                    foreachJoes.Add(employee);
-                }
+                Console.WriteLine(file.ReadToEnd());
             }
 
-            //loop through list of employees and add all employees named "Joe" to a new list
-            //Using lambda function
-            List<Employee> lambdaJoes = employees.Where(name => name.FirstName == "Joe").ToList();
 
-            //loop through list of employees and add all employees with an Id greater than 5 to a new list
-            //Using lambda function
-            List<Employee> idOverFive = employees.Where(id => id.Id > 5).ToList();
-            
+
+
+            ////Creating 10 employee objects with names and ID numbers
+            //Employee employee1 = new Employee() { FirstName = "Joe", LastName = "Smith", Id = 001};
+            //Employee employee2 = new Employee() { FirstName = "Sally", LastName = "Jones", Id = 002 };
+            //Employee employee3 = new Employee() { FirstName = "Ralph", LastName = "Rodgers", Id = 003 };
+            //Employee employee4 = new Employee() { FirstName = "Joe", LastName = "Green", Id = 004 };
+            //Employee employee5 = new Employee() { FirstName = "Bob", LastName = "Allen", Id = 005 };
+            //Employee employee6 = new Employee() { FirstName = "Murphy", LastName = "Palmer", Id = 006 };
+            //Employee employee7 = new Employee() { FirstName = "Steve", LastName = "Stevenson", Id = 007 };
+            //Employee employee8 = new Employee() { FirstName = "Patty", LastName = "Newman", Id = 008 };
+            //Employee employee9 = new Employee() { FirstName = "Shirley", LastName = "Kipling", Id = 009 };
+            //Employee employee10 = new Employee() { FirstName = "Greg", LastName = "Old", Id = 010 };
+
+            ////Add all employees to a list
+            //List<Employee> employees = new List<Employee>
+            //{
+            //    employee1,
+            //    employee2,
+            //    employee3,
+            //    employee4,
+            //    employee5,
+            //    employee6,
+            //    employee7,
+            //    employee8,
+            //    employee9,
+            //    employee10
+            //};
+
+            ////loop through list of employees and add all employees named "Joe" to a new list
+            ////Using foreach
+            //foreach (Employee employee in employees)
+            //{
+            //    if (employee.FirstName == "Joe")
+            //    {
+            //        List<Employee> foreachJoes = new List<Employee>();
+            //        foreachJoes.Add(employee);
+            //    }
+            //}
+
+            ////loop through list of employees and add all employees named "Joe" to a new list
+            ////Using lambda function
+            //List<Employee> lambdaJoes = employees.Where(name => name.FirstName == "Joe").ToList();
+
+            ////loop through list of employees and add all employees with an Id greater than 5 to a new list
+            ////Using lambda function
+            //List<Employee> idOverFive = employees.Where(id => id.Id > 5).ToList();
+
 
 
             ////instantiating an Employee object with type string as it's generic parameter
