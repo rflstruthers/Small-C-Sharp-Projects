d3712a1c08e092f767987a0afd2d69f78844a13f
rflstruthers <rflstruthers@gmail.com>
2019-09-23 13:32:12 -0700

    DateTime Exercise

---
+++ DateTimeExercise/DateTimeExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine("Hello, it is currently {0}", dateTime);
+            Console.WriteLine("\nPlease enter a number of hours:");
+            try
+            {
+                double hours = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("\nIn {0} hours it will be exactly {1}", hours, dateTime.AddHours(hours));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You did not enter a number of hours");
+            }
+            Console.ReadLine();
+        }
+    }
+}
