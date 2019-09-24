cd4ed265e0d3d6563c9a4fc3f815a2a6b322021f
rflstruthers <rflstruthers@gmail.com>
2019-09-24 09:57:12 -0700

    Exception Handling with custom Exception class

---
+++ ExceptionHandlingExercisePart2/ExceptionHandlingExercisePart2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingExercisePart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to ACME Health Insurance." +
+                "\nPlease enter your age to see if you are eligible for our award winning coverage.");
+
+            //if the user enters digits for their age, store that as "age" and print their birth year to the screen
+            //if they enter something other than just digits, throw format exception error
+            //if they enter 0 or negative numbers, throw 
+            bool validAnswer = false;
+            int age = 0;
+            while (!validAnswer)
+            {
+                try
+                {
+                    age = Convert.ToInt32(Console.ReadLine());
+                    validAnswer = true;
+                    if (age <= 0) throw new RejectionException();
+                }
+                catch (RejectionException)
+                {
+                    Console.WriteLine("Your request for coverage has been rejected. Goodbye.");
+                    Console.ReadLine();
+                    return;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please only enter digits for your age.");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("An error has occured. Please come back at a later date or contact Customer Service.");
+                    Console.ReadLine();
+                    return;
+                }
+
+            }
+
+            //gets the current year, subtracts users age from current year to get birth year.
+            int thisYear = DateTime.Now.Year;
+            int birthYear = thisYear - age;
+            Console.WriteLine("According to the information you provided, your birth year is {0}, that means you are eligible for our coverage!", birthYear);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+//bool validAnswer = false;
+//int age = 0;
+//            while (!validAnswer)
+//            {
+//                Console.WriteLine("Please enter your age to see if you are eligible for our award winning coverage.");
+//                validAnswer = int.TryParse(Console.ReadLine(), out age);
+//                if (!validAnswer) Console.WriteLine("Please enter digits only.");
+//            }
+//            Console.WriteLine("\n\n\n\n\n\n" + age);
---
+++ ExceptionHandlingExercisePart2/ExceptionHandlingExercisePart2/RejectionException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingExercisePart2
+{
+    //custom exception class
+    public class RejectionException : Exception
+    {
+        //constructors, inherits base constructor (from Exception class)
+        public RejectionException()
+            : base() { }
+
+        public RejectionException(string message)
+            : base(message) { }
+    }
+}
