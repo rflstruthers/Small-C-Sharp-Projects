cca48123b5d972e8c490a447592ea6c8537252cb
rflstruthers <rflstruthers@gmail.com>
2019-10-02 13:30:24 -0700

    Added MDF and LDF files

--- CarInsuranceMVCExercise/CarInsuranceMVCExercise/Models/CarInsurance.Designer.cs
+++ CarInsuranceMVCExercise/CarInsuranceMVCExercise/Models/CarInsurance.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\User0\source\repos\CarInsuranceMVCExercise\CarInsuranceMVCExercise\Models\CarInsurance.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\User0\Documents\GitHub\Small-C-Sharp-Projects\CarInsuranceMVCExercise\CarInsuranceMVCExercise\Models\CarInsurance.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
