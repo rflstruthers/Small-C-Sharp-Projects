6342676b04806bd96e9cd592ac9dbe65ceeb7149
rflstruthers <rflstruthers@gmail.com>
2019-10-03 10:42:19 -0700

    Code First Entity Framework Exercise

---
+++ CodeFirstEfExercise/CodeFirstEfExercise/DAL/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using CodeFirstEfExercise.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+//Acessing the database we are creating
+namespace CodeFirstEfExercise.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //This line makes the Table names singular ("Student"), without it they will be plural ("Students")
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
